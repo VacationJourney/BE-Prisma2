8753a0041f00014c80debee0dd26885e9456a807
Jeremy McWilliams <j2macwilliams@gmail.com>
2021-01-15 12:04:21 -0800

    change prisma set up for PrismaClient creation bug

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ const { PrismaClient } = require('@prisma/client')
 const resolvers = require('./resolvers/index');
 const typeDefs = require('./typeDefs/index');
 
+const prisma = new PrismaClient()
 const server = new ApolloServer({
 typeDefs,
   resolvers,
   context: req => ({
-      prisma: new PrismaClient(),
+      prisma,
       req
   })
 })
